93021105f6d90af6f1f17cb16a66598a3d9347e3
Enzo DAL  CERRO <edal--ce@e2r2p2.42.fr>
2019-12-03 16:04:51 +0100

    mostly patched

--- get_next_line.c
+++ get_next_line.c
@@ -67,51 +67,74 @@ char  	*refresh_cache(char **cache)
 
 	while((*cache)[i] != '\n' && (*cache)[i] != '\0')
 		i++;
+	//printf("current pos = %d\n", (*cache)[i]);
 	//printf("end char = |%d|\n",(*cache)[i] );
-
+	//printf("Malloc of size %d\n",(ft_strlen(*cache) - i) );
 	out = malloc(sizeof(char) * (ft_strlen(*cache) - i));
 	//Protect malloc
-	if((*cache)[i] != '\n')
-		i++;
-	printf("Current char %d\n", (*cache)[i]);
-	while((*cache)[i] != '\0')
-		out[j++] = (*cache)[i++];
+	//if((*cache)[i] != '\n')
+	//	i++;
+	//printf("Current char %d\n", (*cache)[i]);
+	while((*cache)[++i] != '\0')
+		out[j++] = (*cache)[i];
 	out[j] = '\0';
+	//printf("out = ||%s||\n",out );
 	//printf("out = %s\n",out );
 	//free(*cache);
+	//printf("cache len = %d : %s \n",ft_strlen(out),out );
 	return (out);
 }
 
-char	*get_line_from_cache(char **cache, int read_status)
+char	*get_line_from_cache(char **cache, int *read_status)
 {
 	int i;
 	char *out;
-	char *tmp;
+	//char *tmp;
 	int j;
 
 	out = NULL;
 	j = 0;
 	i = ft_strchr_int(*cache, '\n');
-	if(read_status == 0 && i == -1)
-	 	i = ft_strchr_int(*cache, '\0');
+	if(*read_status == 0 && i == -1)
+	{
+	 	//printf("we go in\n");
+		i = ft_strchr_int(*cache, '\0');
+		//printf("I equal %d\n", i);
+	}
 	if(i != -1)
 	{
 		out = malloc(sizeof(char) * (i + 1));
+		//printf("i = %d \n",i );
 		while(j < i)
 		{
 			out[j] = (*cache)[j];
 			j++;
 		}
+		if(i == 0 && (*cache)[i] != '\0' && *read_status == 0)
+		{	
+			//printf("That's the case\n");
+			//printf("ft_strlen cache %d\n",ft_strlen(*cache) );
+			*read_status = 1;
+			//out[j++] = '\n';
+		}
 		out[j] = '\0';
-		
+		//printf("Cache b4 rds %d %d ; |%s| \n",*read_status,ft_strlen(*cache),*cache );
+		//printf(" cache len b4%d \n" ,ft_strlen(*cache));
 		*cache = refresh_cache(cache);
+		//printf("Cache af %d ; |%s| \n",ft_strlen(*cache),*cache );
 		//i++;
 		//tmp = ft_strdup((const char *) (*cache + i));
 		//free(*cache);
 		//*cache = tmp;
 	}
 	else
+	{	
+		// /printf("i == -1 \n" );
 		return (NULL);
+	}
+	
+	if(ft_strlen(*cache) > 0)
+		*read_status = 1;
 	return(out);
 }
 
@@ -132,16 +155,18 @@ int		get_next_line(int fd, char **line)
 		read_status = read(fd, buffer, BUFFER_SIZE);
 		tmp = ft_strjoin_buff(cache, buffer);
 		cache = tmp;
-		*line = get_line_from_cache(&cache, read_status);
+		*line = get_line_from_cache(&cache, &read_status);
+		
 	}	
-	if(ft_strlen(cache) > 0)
-	{
-		//printf("Sortie avec cache > 0\n");
-		return 1;
-	}
-	else
-	{
-		//printf("Sortie avec cache =< 0\n");
-		return read_status;
-	}
+	// if(ft_strlen(cache) > 0)
+	// {
+	// 	//printf("Sortie avec cache > 0\n");
+	// //å	return 1;
+	// }
+	// else
+	// {
+	//printf("status ; %d\n",read_status);
+	//printf("cache b4 ret |%s| \n",cache);
+	// }
+	return read_status;
 }
--- main.c
+++ main.c
@@ -8,21 +8,24 @@ int main(int ac, char *av[ac])
 	int fd = open(av[1], O_RDONLY);
 	//read(0, buf, 1);
 	int status = 1;
-	char *test = ft_strdup("This is the sample text first \n second line, two empty \n\n and a final one");
-	printf("|%s|\n",test );
-	printf("|%s|\n",refresh_cache(&test));
-	printf("|%s|\n",refresh_cache(&test));
-	printf("|%s|\n",refresh_cache(&test));
- // 	while(status != 0)
-	// {
-	//  	status = get_next_line(fd, &line);
-	// 	printf("|%s\n", line);
-	// 	//free(line);
-	// }
-	//printf("BOUCLE FIN\n%s\n", line);
+//	char *test = ft_strdup("This is the sample text first \n second line, two empty \n\n and a final one");
+	// printf("|%s|\n",test);
+	// test = refresh_cache(&test);
+	// printf("|%s|\n",test);
+	// test = refresh_cache(&test);
+	// printf("|%s|\n",test);
+	// test = refresh_cache(&test);
+	// printf("|%s|\n",test);
+	while(status != 0)
+	{
+	  	status = get_next_line(fd, &line);
+	 	printf("|%s\n", line);
+		free(line);
+	}
+	//printf("BOUCLE FIN |%s\n", line);
 	//status = get_next_line(fd, &line);
 	//printf("BOUCLE FIN\n%s\n", line);
-	free(line);
+	//free(line);
 	close(fd);
 	return 0;
 }
